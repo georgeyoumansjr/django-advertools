1ffb96340e76dbd0326a6b0ef1898285aae3db56
georgeyoumansjr <georgeyoumansjr@gmail.com>
2023-06-16 10:08:44 -0400

    setup things

--- django_advertools/asgi.py
+++ django_advertools/asgi.py
@@ -4,7 +4,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from seo import routing  # Replace 'myapp' with the name of your app
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '<your_project_name>.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_advertools.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
