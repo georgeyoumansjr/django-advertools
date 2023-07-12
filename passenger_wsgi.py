9ddb5a5cd825a7b5d2be4bb64b237d1dc05c4182
georgeyoumansjr <georgeyoumansjr@gmail.com>
2023-07-12 09:57:16 -0400

    remv circ import error from passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,1 +1,8 @@
-from django_advertools.wsgi import application
+import imp
+import os 
+import sys 
+
+sys.path.insert(0, os.path.dirname(__file___))
+
+wsgi = imp.load_source('wsgi', 'django_advertools/wsgi.py')
+application = wsgi.application
